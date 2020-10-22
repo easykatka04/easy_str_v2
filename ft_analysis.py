dbe7f51e186476a8418e3cca8c8bc2a3e32c2dba
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-10-22 12:43:32 +0300

    Add files via upload

--- ft_analysis.py
+++ ft_analysis.py
@@ -1,6 +1,40 @@
-def ft_count_words(sstr):
-    count = 1
-    for i in sstr:
-        if i == ' ':
-            count += 1
-    return count
+def ft_len(a):
+    c = 0
+    for i in a:
+        c += 1
+    return (c)
+
+
+def ft_analysis(a):
+    print(a[2])
+    print(a[ft_len(a) - 2])
+    print(a[0], a[1], a[2], a[3], a[4], sep="")
+    i = 0
+    while i != ft_len(a) - 2:
+        print(a[i], end="")
+        i += 1
+    i = 0
+    print(end="\n")
+    while i != ft_len(a):
+        if i % 2 == 0:
+            print(a[i], end="")
+        i += 1
+    print(end="\n")
+    i = 0
+    while i != ft_len(a):
+        if i % 2 != 0:
+            print(a[i], end="")
+        i += 1
+    print(end="\n")
+    i = 0
+    while i != ft_len(a):
+        print(a[ft_len(a) - i - 1], end="")
+        i += 1
+    print(end='\n')
+    i = 0
+    while i != ft_len(a):
+        if i % 2 == 0:
+            print(a[ft_len(a) - i - 1], end="")
+        i += 1
+    print(end='\n')
+    print(ft_len(a))
